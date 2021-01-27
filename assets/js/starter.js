29d5c71905b237a519bc06493fa72fbc257855e9
vladmashtak <vladmashtak@gmail.com>
2021-01-27 13:22:33 +0200

    change external scripts

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -18,11 +18,11 @@
 // If you're importing tooltips or popovers, be sure to include our Popper.js dependency
 // import "../../node_modules/popper.js/dist/popper.min.js";
 
-import "../../node_modules/jquery/dist/jquery.slim.min.js";
+// import "../../node_modules/jquery/dist/jquery.slim.min.js";
 // import "../../node_modules/bootstrap/js/dist/util.js";
 // import "../../node_modules/bootstrap/js/dist/collapse.js";
 // import "../../node_modules/bootstrap/js/dist/modal.js";
-import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+// import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 
 
